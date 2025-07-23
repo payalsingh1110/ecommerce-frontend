63f845d4c0d3a2a647706f43a9b326a35f66e492
payalsingh1110 <payalsingh.111020@gmail.com>
2025-07-23 14:45:06 +0530

    Frontend:  Add/Delete product integrated, UI stable, minor layout bugs remain

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../service/admin.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +12,18 @@ import { AdminService } from '../../service/admin.service';
 export class DashboardComponent {
 
   products: any[]= [];
+  searchProductForm!: FormGroup;
+  
 
-  constructor(private adminService: AdminService){}
+  constructor(private adminService: AdminService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar, ){}
 
   ngOnInit(){
     this.getAllProducts();
+    this.searchProductForm = this.fb.group({
+      title: ['', Validators.required]
+    })
   }
 
   //method which call the API
@@ -27,5 +36,30 @@ export class DashboardComponent {
       });
     })
   }
+
+  submitForm(){
+    this.products= [];
+    const title = this.searchProductForm.get('title')!.value;
+    this.adminService.getAllProductsByName(title).subscribe(res=>{
+      res.forEach(element =>{
+        element.processImg = 'data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+    })
+
+  }
+
+  deleteProduct(productId:any){
+    this.adminService.deleteProduct(productId).subscribe(res=>{
+      if(res.body == null){
+        this.snackBar.open('Product Deleted Successfully!', 'Close', {duration: 5000});
+        this.getAllProducts();
+      }else{
+        this.snackBar.open(res.message, 'Close', {duration: 5000, panelClass: 'error-snackbar'});
+      }
+    })
+  }
+
+
 
 }
--- src/app/admin/service/admin.service.ts
+++ src/app/admin/service/admin.service.ts
@@ -31,8 +31,20 @@ constructor(private http: HttpClient) { }
       })
   }
 
-   getAllProducts() : Observable<any>{
+  getAllProducts() : Observable<any>{
       return this.http.get(BASIC_URL + 'api/admin/products', {
+        headers : this.createAuthorizationHeader(),
+      })
+  }
+
+  getAllProductsByName(name: any) : Observable<any>{
+      return this.http.get(BASIC_URL + `api/admin/search/${name}`, {
+        headers : this.createAuthorizationHeader(),
+      })
+  }
+
+  deleteProduct(productId:any) : Observable<any>{
+      return this.http.post(BASIC_URL + `api/admin/product/${productId}`, {
         headers : this.createAuthorizationHeader(),
       })
   }
