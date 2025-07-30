0f19a086e9898cc49f765fee3b9056c7273e1b40
payalsingh1110 <payalsingh.111020@gmail.com>
2025-07-30 15:00:03 +0530

    feat:track order by trackingId

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { TrackOrderComponent } from './track-order/track-order.component';
 
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
+  { path: "order", component: TrackOrderComponent },
   { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
  //{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
